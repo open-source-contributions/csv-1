c90d512b8484fb80b0967b295512f765fb3ff9e0
odan <odan@users.noreply.github.com>
2018-02-06 15:34:49 +0100

    Added tests

---
+++ tests/CsvReaderTest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Test;
+
+use Odan\Csv\CsvReader;
+use org\bovigo\vfs\vfsStream;
+use org\bovigo\vfs\vfsStreamDirectory;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * CsvReaderTest
+ *
+ * @coversDefaultClass \Odan\Csv\CsvReader
+ */
+class CsvReaderTest extends TestCase
+{
+
+    /**
+     * @var vfsStreamDirectory
+     */
+    protected $root;
+
+    protected function setUp()
+    {
+        $this->root = vfsStream::setup('root');
+    }
+
+    /**
+     * Test create object.
+     *
+     * @return void
+     * @covers ::__construct
+     */
+    public function testInstance()
+    {
+        $this->assertInstanceOf(CsvReader::class, new CsvReader());
+    }
+}
---
+++ tests/CsvWriterTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Test;
+
+use Odan\Csv\CsvWriter;
+use org\bovigo\vfs\vfsStream;
+use org\bovigo\vfs\vfsStreamDirectory;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * CsvWriterTest
+ *
+ * @coversDefaultClass \Odan\Csv\CsvWriter
+ */
+class CsvWriterTest extends TestCase
+{
+
+    /**
+     * @var vfsStreamDirectory
+     */
+    protected $root;
+
+    protected function setUp()
+    {
+        $this->root = vfsStream::setup('root');
+    }
+
+    /**
+     * Test create object.
+     *
+     * @return void
+     * @covers ::__construct
+     */
+    public function testInstance()
+    {
+        $file = vfsStream::url('root/output.csv');
+        $this->assertInstanceOf(CsvWriter::class, new CsvWriter($file));
+    }
+}
